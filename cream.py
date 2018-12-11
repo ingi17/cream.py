dfeb37ae4430bf700421ec86acc4bfde55b7294e
Ingi <ingi17@ru.is>
2018-12-11 18:08:16 +0000

    Flokka þáttanöfn í lista miðað við keyword í nafni

--- cream.py
+++ cream.py
@@ -8,9 +8,35 @@ filenames = []
 folders = []
 path = []
 
-for (dirpath, dirnames, fnames) in walk(folder):
-    filenames.extend(fnames)
-    folders.extend(dirnames)
-    path.append(dirpath)
+def setlists(folder):
+    for (dirpath, dirnames, fnames) in walk(folder):
+        filenames.extend(fnames)
+        folders.extend(dirnames)
+        path.append(dirpath)
 
-print(path)
+setlists(folder)
+
+def getSeries(fnames):
+    names = []
+    for f in fnames:
+        keyword = "s0"
+        if keyword in f.lower():
+            names.append(f.lower().split(keyword,1)[0])
+        
+        keyword = "s1"
+        if keyword in f.lower():
+            names.append(f.lower().split(keyword,1)[0])
+
+        keyword = "season"
+        if keyword in f.lower():
+            names.append(f.lower().split(keyword,1)[0])
+    return names
+
+names = getSeries(filenames)
+names = list(set(names))
+
+print(names)
+# keyra i gegnum lista og finna nöfn þátta út frá keyword "season"
+# keyword "sxx"
+# keyword "sxxexx"
+# keyword "xxxx"
