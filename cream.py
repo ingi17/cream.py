2a5713574fe8cef143e7f37c052c2af52da6238f
maggi321 <magsterinn@gmail.com>
2018-12-12 17:29:13 +0000

    ignore a ákveðna file, readme update, gitignore bætt við

--- cream.py
+++ cream.py
@@ -9,27 +9,37 @@ filenames = []
 folders = []
 path = []
 
-def setlists(folder):
+def setLists(folder):
     for (dirpath, dirnames, fnames) in walk(folder):
         filenames.extend(fnames)
         folders.extend(dirnames)
         path.append(dirpath)
 
-setlists(folder)
+setLists(folder)
+def ingoreFiles(file):
+    if re.search(r'.png$|.dat$|.nfo$|.jpg$|.url$|.URL$|.txt$|(s|S)ample', file):
+        return False
+    return True
 
 def getSeries(fnames):
-    regex = re.compile('((s|S)\d+)|(\d+(x|X)\d{1,2})|((e|E)\d+)|((S|s)eason (\d+|I+))')
+    regex = re.compile(r'((s|S)\d+)|(\d+(x|X)\d{1,2})|((e|E)\d+)|((S|s)eason (\d+|I+))')
     names = filter(regex.search, fnames)
 
     nlist = []
     for f in names:
-        nlist.append(regex.split(f)[0])
+        #print(f)
+        #if re.search('.png$|.dat$|.nfo$|.jpg$|.url$|.URL$|.txt$|(s|S)ample', f):
+         #   pass
+        #else:
+        if ingoreFiles(f):
+            nlist.append(regex.split(f)[0])
     return nlist
 
 names = getSeries(filenames)
 names = list(set(names))
 names.sort()
-
+#for i in names:
+ #   print(i)
 print(names)
 # keyra i gegnum lista og finna nöfn þátta út frá keyword "season"
 # keyword "sxx"
