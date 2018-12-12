4b5d745a309c45ec7f905d9233acf9550b1ad0d8
Ingi <ingi17@ru.is>
2018-12-12 17:57:05 +0000

    Auka fix frá merge-i

--- cream.py
+++ cream.py
@@ -16,30 +16,26 @@ def setLists(folder):
         path.append(dirpath)
 
 setLists(folder)
-def ingoreFiles(file):
+
+def ignoreFiles(file):
     if re.search(r'.png$|.dat$|.nfo$|.jpg$|.url$|.URL$|.txt$|(s|S)ample', file):
         return False
     return True
 
-
 def getSeries(fnames):
-<<<<<<< HEAD
-    regex = re.compile('((s|S)\d{1,2})|(\d{1,2}(x|X)\d{1,2})|((e|E)\d{1,2})|((S|s)eason (\d+|I+))')
-=======
-    regex = re.compile(r'((s|S)\d+)|(\d+(x|X)\d{1,2})|((e|E)\d+)|((S|s)eason (\d+|I+))')
->>>>>>> 2a5713574fe8cef143e7f37c052c2af52da6238f
+    regex = re.compile(r'((s|S)\d{1,2})|(\d{1,2}(x|X)\d{1,2})|((e|E)\d{1,2})|((S|s)eason (\d+|I+))')
     names = filter(regex.search, fnames)
 
     nlist = []
     chars = ['.', '-', '[']
     for f in names:
-<<<<<<< HEAD
-        show = regex.split(f)[0]
+        if ignoreFiles(f):
+            show = regex.split(f)[0]
 
-        for c in chars:
-            show = show.replace(c, ' ')
+            for c in chars:
+                show = show.replace(c, ' ')
 
-        nlist.append(show.strip())
+            nlist.append(show.strip())
 
     shows = list(set(nlist))
     shows.sort()
@@ -51,25 +47,3 @@ print(getSeries(filenames))
 
 def makeDirs():
     return 0
-
-
-=======
-        #print(f)
-        #if re.search('.png$|.dat$|.nfo$|.jpg$|.url$|.URL$|.txt$|(s|S)ample', f):
-         #   pass
-        #else:
-        if ingoreFiles(f):
-            nlist.append(regex.split(f)[0])
-    return nlist
-
-names = getSeries(filenames)
-names = list(set(names))
-names.sort()
-#for i in names:
- #   print(i)
-print(names)
->>>>>>> 2a5713574fe8cef143e7f37c052c2af52da6238f
-# keyra i gegnum lista og finna nöfn þátta út frá keyword "season"
-# keyword "sxx"
-# keyword "sxxexx"
-# keyword "xxxx"
