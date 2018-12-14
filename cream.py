f2f63af0b97e68eb45055a63fceb241184778e6a
maggi321 <magsterinn@gmail.com>
2018-12-14 14:30:48 +0000

    breytt lista í dict og skrifar í txt file

--- cream.py
+++ cream.py
@@ -1,12 +1,13 @@
-import sys, re, os
+import sys, re, os, json
 from os import walk
 
 folder = sys.argv[1]
-target = sys.argv[2]
+#target = sys.argv[2]
 
 filenames = []
 folders = []
 path = []
+dictoTest = {}
 
 def setLists(folder):
     for (dirpath, dirnames, fnames) in walk(folder):
@@ -38,14 +39,22 @@ def getSeries(fnames):
             show = regex.split(f)[0]
             for c in chars:
                 show = show.replace(c, ' ')
-            show = show.lower()
+                show = re.sub('  +', ' ', show)
+            show = show.lower().strip()
             show = show.title()
+            if show not in dictoTest:
+                dictoTest[show] = [f]
+            elif show in dictoTest:
+                if f not in dictoTest[show]:
+                    dictoTest[show].append(f) 
             nlist.append(show.strip())
-
     shows = list(set(filter(None, nlist)))
     shows.sort()
-    print(len(shows))
-    return shows
+    #print(len(shows))
+    dictToTxt(dictoTest)
+    listToTxt(dictoTest)
+    #return dictoTest
+    #return shows
 
 def makeDirs(shows):
     if not os.path.exists(target):
@@ -56,6 +65,20 @@ def makeDirs(shows):
         if not os.path.exists(dirs):
             os.makedirs(dirs)
 
+def dictToTxt(dicto):
+    with open('dictListi.txt', 'w') as file:
+        for k, v in dicto.items():
+            file.write(str(k) + ' >>> '+ str(v) + '\n\n')
+        #file.write(json.dumps(dicto))
+    #f = open('listi.txt', 'w+')
+    #for i in dicto:
+     #   print(i)
+      #  f.write(i)
+    #f.close()
+def listToTxt(listo):
+    with open('keyListi.txt', 'w') as file:
+        for k in listo:
+            file.write(k + '\n\n')
 setLists(folder)
 print(getSeries(filenames))
 #makeDirs(getSeries(filenames))
