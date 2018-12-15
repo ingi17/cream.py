b053107d6b4f3f9fb85d6c54d142087e935a8cb6
Ingi <ingi17@ru.is>
2018-12-15 22:34:30 +0000

    Code cleanup

--- cream.py
+++ cream.py
@@ -1,4 +1,7 @@
-import sys, re, os, shutil
+import sys
+import re
+import os
+import shutil
 from os import walk
 
 folder = sys.argv[1]
@@ -7,7 +10,6 @@ target = sys.argv[2]
 filenames = []
 folders = []
 path = []
-dictoTest = {}
 
 def setLists(folder):
     for (dirpath, dirnames, fnames) in walk(folder):
@@ -21,56 +23,56 @@ def fileExtensions(file):
     elif re.search(r'.mkv$|.flv$|.avi$|.wmv$|.mov$|.mp4$|.mpg$|.mpeg$|.nfv$|.mp3$', file):
         return True
     return False
+
     #if re.search(r'.png$|.dat$|.nfo$|.jpg$|.url$|.URL$|.txt$|(s|S)ample', file):
      #   return False
     #return True
 
 def getSeries(fnames):
+    showDic = {}
     regex = re.compile(r'((s|S)\d{1,2})|(\s\d{1,2}(x|X)\d{1,2})|((e|E)\d{1,2})|((S|s)eason (\d+|I+))|(^|\s)\d{2,3}(\s|[*p])')
-    names = filter(regex.search, fnames)
-
-    nlist = []
     chars = ['.', '-', '[', '_']
+    names = filter(regex.search, fnames)
 
     for f in names:
         if fileExtensions(f):
+            
             show = regex.split(f)[0]
+            
             for c in chars:
                 show = show.replace(c, ' ')
                 show = re.sub('  +', ' ', show)
+            
             show = show.lower().strip()
             show = show.title()
-            if show not in dictoTest:
-                dictoTest[show] = [f]
-            elif show in dictoTest:
-                if f not in dictoTest[show]:
-                    dictoTest[show].append(f) 
-            nlist.append(show.strip())
-    shows = list(set(filter(None, nlist)))
-    shows.sort()
-    #print(len(shows))
-    dictToTxt(dictoTest)
-    listToTxt(dictoTest)
-    return dictoTest
-    #return shows
+            
+            if show not in showDic:
+                showDic[show] = [f]
+            
+            elif show in showDic:
+                if f not in showDic[show]:
+                    showDic[show].append(f) 
 
-def makeDirs(shows):
+    dictToTxt(showDic)
+    listToTxt(showDic)
+
+    return showDic
+
+def makeDirs(showDic):
     if not os.path.exists(target):
         os.makedirs(target)
 
-    for key in dictoTest:
+    for key in showDic:
         dirs = target + "/" + key + "/"
+
         if not os.path.exists(dirs):
             os.makedirs(dirs)
-            for show in dictoTest[key]:
-                #print(show)
+            
+            for show in showDic[key]:
                 for i in path:
-                    #print(show)
-                    if 'Person.of.Interest.S04E19.HDTV.x264-LOL[ettv]' in i:
-                        print(i)
-                  #  print(move)
-                #print(i)
-
+                    if i.find(show):
+                        continue
+                        
 def dictToTxt(dicto):
     with open('dictListi.txt', 'w') as file:
         for k, v in dicto.items():
@@ -80,7 +82,8 @@ def listToTxt(listo):
     with open('keyListi.txt', 'w') as file:
         for k in listo:
             file.write(k + '\n\n')
+
 setLists(folder)
 #print(getSeries(filenames))
-makeDirs(getSeries(filenames))
+print(makeDirs(getSeries(filenames)))
 #print(path)
